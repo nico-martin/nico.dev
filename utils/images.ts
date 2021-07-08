ade9da75fb38e3944efcbd3fed769533defdd785
Nico Martin <mail@nicomartin.ch>
2021-07-08 13:13:02 +0200

    improved image format check

--- utils/images.ts
+++ utils/images.ts
@@ -1,30 +1,26 @@
 export const supportedImageFormat = (): Promise<'avif' | 'webp' | 'jpg'> =>
   new Promise((resolve, reject) => {
     const avifData =
-      'data:image/avif;base64,AAAAFGZ0eXBhdmlmAAAAAG1pZjEAAACgbWV0YQAAAAAAAAAOcGl0bQAAAAAAAQAAAB5pbG9jAAAAAEQAAAEAAQAAAAEAAAC8AAAAGwAAACNpaW5mAAAAAAABAAAAFWluZmUCAAAAAAEAAGF2MDEAAAAARWlwcnAAAAAoaXBjbwAAABRpc3BlAAAAAAAAAAQAAAAEAAAADGF2MUOBAAAAAAAAFWlwbWEAAAAAAAAAAQABAgECAAAAI21kYXQSAAoIP8R8hAQ0BUAyDWeeUy0JG+QAACANEkA=';
+      'data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A=';
     const webpData =
-      'data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoCAAEAAQAcJaQAA3AA/v3AgAA=';
+      'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
 
-    const checkAviv = () =>
+    const checkSupport = (imgData: string): Promise<string> =>
       new Promise((resolve, reject) => {
-        fetch(avifData)
-          .then((r) => r.blob())
-          .then((blob) =>
-            createImageBitmap(blob).then(
-              () => resolve('avif support'),
-              () => reject()
-            )
-          )
-          .catch(() => reject());
+        const img = new Image();
+        img.onload = img.onerror = () =>
+          img.height === 2 ? resolve('') : reject();
+        img.src = imgData;
       });
 
-    Promise.allSettled([checkAviv(), fetch(webpData)]).then(([avif, webp]) => {
-      resolve(
-        avif.status === 'fulfilled'
-          ? 'avif'
-          : webp.status === 'fulfilled'
-          ? 'webp'
-          : 'jpg'
-      );
-    });
+    Promise.allSettled([checkSupport(avifData), checkSupport(webpData)]).then(
+      ([avif, webp]) =>
+        resolve(
+          avif.status === 'fulfilled'
+            ? 'avif'
+            : webp.status === 'fulfilled'
+            ? 'webp'
+            : 'jpg'
+        )
+    );
   });
