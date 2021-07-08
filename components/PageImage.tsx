0abb9464d761210c03ca50e29d52fd35449656e5
Nico Martin <mail@nicomartin.ch>
2021-07-08 12:44:04 +0200

    added check for image type

--- components/PageImage.tsx
+++ components/PageImage.tsx
@@ -4,11 +4,6 @@ import { supportedImageFormat } from '@utils/images';
 import styles from './PageImage.module.css';
 
 const SIZES = [500, 700, 1000, 1500, 2000];
-const TYPES = {
-  //avif: 'image/avif',
-  webp: 'image/webp',
-  jpg: '',
-};
 
 const PageImage = ({ className = '' }: { className?: string }) => {
   const [imageExt, setImageExt] = React.useState<'avif' | 'webp' | 'jpg'>(null);
@@ -24,7 +19,7 @@ const PageImage = ({ className = '' }: { className?: string }) => {
     >
       {imageExt && (
         <img
-          alt="Nico Martin - Frontend Developer"
+          alt={`Nico Martin - Frontend Developer - ${imageExt}`}
           className={cn(styles.image)}
           srcSet={SIZES.map(
             (size) => `../portrait/nico-martin-${size}.${imageExt} ${size}w`
