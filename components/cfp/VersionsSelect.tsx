cfc8264b5592640d5341393ee57d896b7d7d7933
Nico Martin <mail@nicomartin.ch>
2023-06-05 23:35:33 +0200

    added cfp page

---
+++ components/cfp/VersionsSelect.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import CopyP from '@comps/cfp/CopyP';
+import cn from '@utils/classnames';
+import styles from './VersionsSelect.module.css';
+
+const VersionsSelect: React.FC<{
+  className?: string;
+  options: Array<{ title: string; content: string }>;
+}> = ({ className = '', options }) => {
+  const [activeOption, setActiveOption] = React.useState<number>(0);
+  const activeContent: string = React.useMemo(
+    () => options[activeOption].content,
+    [activeOption]
+  );
+
+  return (
+    <div className={cn(className, styles.root)}>
+      <label className={styles.label}>
+        <select
+          className={styles.select}
+          onChange={(e) => setActiveOption(parseInt(e.target.value))}
+        >
+          {options.map(({ title, content }, i) => (
+            <option value={i}>
+              {title} ({content.length})
+            </option>
+          ))}
+        </select>
+      </label>
+      <div className={styles.content}>
+        <CopyP content={activeContent} />
+        <p className={styles.size}>
+          {activeContent.length} / {activeContent.split(' ').length}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default VersionsSelect;
--- pages/cfp.tsx
+++ pages/cfp.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { InferGetServerSidePropsType } from 'next';
 import Head from 'next/head';
 import PageContent from '@comps/PageContent';
-import { getPageProps } from '@utils/helpers';
+import CopyP from '@comps/cfp/CopyP';
+import Papers from '@comps/cfp/Papers';
+import VersionsSelect from '@comps/cfp/VersionsSelect';
+import { getCfp } from '@utils/helpers';
+import styles from './cfp.module.css';
 
-export const getStaticProps = async () => ({});
+export const getStaticProps = async () => await getCfp();
 
-export default ({}: InferGetServerSidePropsType<typeof getStaticProps>) => (
+export default ({
+  pageData,
+}: InferGetServerSidePropsType<typeof getStaticProps>) => (
   <React.Fragment>
     <Head>
       <title key="title">CFP - Nico Martin</title>
@@ -21,6 +27,34 @@ export default ({}: InferGetServerSidePropsType<typeof getStaticProps>) => (
       title="CFP"
       //html={pageData.content}
       //intro={pageData.intro}
-    />
+      classNameContent={styles.content}
+    >
+      <h2>About</h2>
+      <VersionsSelect
+        options={pageData.about.map((version) => ({
+          title: version.title,
+          content: version.text,
+        }))}
+      />
+      <h2>Links</h2>
+      <div className={styles.links}>
+        {pageData.links.map((link) => (
+          <div className={styles.link}>
+            <span>{link.title}</span>
+            <CopyP content={link.url} />
+          </div>
+        ))}
+      </div>
+      <h2>Images</h2>
+      <div className={styles.images}>
+        {pageData.portrait.map((image, i) => (
+          <div className={styles.image}>
+            <CopyP content={image} key={i} />
+          </div>
+        ))}
+      </div>
+      <h2>Papers</h2>
+      <Papers papers={pageData.papers} />
+    </PageContent>
   </React.Fragment>
 );
