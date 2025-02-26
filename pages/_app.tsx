b7dc2c6182b8c25f0cfc1d6c8dbe6548d63bd318
Nico Martin <mail@nico.dev>
2025-02-26 15:19:25 +0100

    fixed og images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -98,20 +98,8 @@ export default ({ Component, pageProps }) => {
         <meta name="theme-color" content="#009d89" />
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content={
-            router.route === '/talks'
-              ? './facebook-talks.png'
-              : './facebook.png'
-          }
-        />
-        <meta
-          name="twitter:image"
-          content={
-            router.route === '/talks' ? './twitter-talks.png' : './twitter.png'
-          }
-        />
+        <meta property="og:image" content="./og.png" />
+        <meta name="twitter:image" content="./twitter.png" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <PageLayout>
